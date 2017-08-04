d6fda2eab0260a6cec53fcd43db71966f9c8a8d1
BIT <BIT@DESKTOP-DOGVD1T>
2017-08-04 10:34:51 +0900

    방명록 등록 완성

--- src/com/guestbook/controller/GuestBookController.java
+++ src/com/guestbook/controller/GuestBookController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.guestbook.dao.GuestBookDao;
 import com.guestbook.vo.GuestBookVo;
@@ -20,6 +22,18 @@ public class GuestBookController {
 		List<GuestBookVo> list = dao.getlist();
 		model.addAttribute("list", list);
 		return "list";
+	}
+	
+	@RequestMapping("/add")
+	public String add(@ModelAttribute GuestBookVo vo ) {
+		dao.insert(vo);
+		return "redirect:/list";
+	}
+	
+	@RequestMapping("deleteform")
+	public String deleteform(@RequestParam("no") int no, Model model) {
+		model.addAttribute(no);
+		return "deleteform";
 	}
 
 }
--- webapp/WEB-INF/views/list.jsp
+++ webapp/WEB-INF/views/list.jsp
@@ -7,11 +7,11 @@
 <title>방명록</title>
 </head>
 <body>
-	<form action="/g2/gb?a=add" method="post">
+	<form action="/g3/add" method="post">
 	<table border=1 width=500>
 		<tr>
 			<td>이름</td><td><input type="text" name="name"></td>
-			<td>비밀번호</td><td><input type="password" name="password"></td>
+			<td>비밀번호</td><td><input type="password" name="pw"></td>
 		</tr>
 		<tr>
 			<td colspan=4><textarea name="content" cols=60 rows=5></textarea></td>
