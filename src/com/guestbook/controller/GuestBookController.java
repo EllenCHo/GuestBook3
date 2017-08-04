bbe6a4cc76da7e905f4187864b07e41c4af2b1aa
BIT <BIT@DESKTOP-DOGVD1T>
2017-08-04 10:13:25 +0900

    dao를 사용하기 위한 세팅

--- src/com/guestbook/controller/GuestBookController.java
+++ src/com/guestbook/controller/GuestBookController.java
@@ -1,5 +1,13 @@
 package com.guestbook.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GuestBookController {
+	@RequestMapping("/list")
+	public String list() {
+		return null;
+	}
 
 }
